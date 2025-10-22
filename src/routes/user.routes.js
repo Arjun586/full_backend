b3a4977a2e92620e6ea65d66a427d263cdc64888
Arjun <121486212+Arjun586@users.noreply.github.com>
2025-10-23 03:00:00 +0530

    Initial commit

---
+++ src/routes/user.routes.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import {loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
+import { verfiyJWT } from "../middlewares/auth.middleware.js";
+
+
+const router = Router()
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
+
+router.route("/login").post(loginUser)
+
+// secured Routes
+router.route("/logout").post(verfiyJWT, logoutUser)
+router.route("/refreshToken").post(refreshAccessToken)
+
+
+export default router
